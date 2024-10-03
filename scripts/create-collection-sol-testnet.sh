59a4cf3bc8a99b8c3aa08938ca2a48f98865d0d6
Vadim Fadeev <xboxfadeev@gmail.com>
2024-10-03 13:15:38 +0100

    feat: add new deploy

--- scripts/create-collection-sol-testnet.sh
+++ scripts/create-collection-sol-testnet.sh
@@ -19,10 +19,10 @@ npx ts-node ./src/cli/controls/createDeployment.ts \
 
 
 
-npx ts-node ./src/cli/controls/addPhase.ts -d 9jkje89GCXpKpGYe6vU8HxawVLtB37nLgwTWo1QNGbQN -k ~/.config/solana/id.json -r https://api.devnet.solana.com  --maxMintsPerWallet 100 --maxMintsTotal 1150 --priceAmount 0 -s 1709564319 -e 1959564319
+npx ts-node ./src/cli/controls/addPhase.ts -d AmqfVpkT986fqy3q9PbdXKfBpUwg3ikS2ij1AEDVBvb -k ~/.config/solana/id.json -r https://api.devnet.solana.com  --maxMintsPerWallet 100 --maxMintsTotal 1150 --priceAmount 0 -s 1709564319 -e 1959564319
 
 
-npx ts-node ./src/cli/controls/mintWithControls.ts -d 9jkje89GCXpKpGYe6vU8HxawVLtB37nLgwTWo1QNGbQN -k ~/.config/solana/id.json -r https://api.devnet.solana.com  -p 0 -n 1
+npx ts-node ./src/cli/controls/mintWithControls.ts -d AmqfVpkT986fqy3q9PbdXKfBpUwg3ikS2ij1AEDVBvb -k ~/.config/solana/id.json -r https://api.devnet.solana.com  -p 0 -n 1
 
 
 # modify royalties
@@ -41,13 +41,13 @@ npx ts-node ./src/cli/controls/createDeployment.ts \
   --creators 6H7iu5V5q2j3G4hjKj6HGFEdUJGZn7MBXaxCjEHD2A4a:50 7Y7bfS5qE2i4J3hjLs1JKJFRXoJHdF2bCjCzTGbJ3h3K:50 \
   --royaltyBasisPoints 1000 \
   --platformFeeValue 500000 \
-  --platformFeeRecipients 6H7iu5V5q2j3G4hjKj6HGFEdUJGZn7MBXaxCjEHD2A4a:50 7Y7bfS5qE2i4J3hjLs1JKJFRXoJHdF2bCjCzTGbJ3h3K:50 \
+  --platformFeeRecipients 674s1Sap3KVnr8WGrY5KGQ69oTYjjgr1disKJo6GpTYw:100 \
   --isFeeFlat \
   --extraMeta "field1:value1" "field2:value2" "field3:value3" "field4:value4" \
   --itemBaseUri "ipfs://QmbsXNSkPUtYNmKfYw1mUSVuz9QU8nhu7YvzM1aAQsv6xw/{}" \
   --itemName "Item T8 for V3 #{}"
 
-npx ts-node ./src/cli/controls/addPhase.ts -d AEDyi3Cf89zxADzPbT1azqJo1c5LfSoBkhFSd3aPSGYJ -k ~/.config/solana/id.json -r https://testnet.dev2.eclipsenetwork.xyz  --maxMintsPerWallet 100 --maxMintsTotal 1150 --priceAmount 0 -s 1709564319 -e 1959564319
+npx ts-node ./src/cli/controls/addPhase.ts -d GdyxiM4D36YqR84aoBfWiAtUciBVbXugwjZJnuVe2dQN -k ~/.config/solana/id.json -r https://testnet.dev2.eclipsenetwork.xyz  --maxMintsPerWallet 100 --maxMintsTotal 1150 --priceAmount 0 -s 1709564319 -e 1959564319
 
 
-npx ts-node ./src/cli/controls/mintWithControls.ts -d AEDyi3Cf89zxADzPbT1azqJo1c5LfSoBkhFSd3aPSGYJ -k ~/.config/solana/id.json -r https://testnet.dev2.eclipsenetwork.xyz  -p 0 -n 1
+npx ts-node ./src/cli/controls/mintWithControls.ts -d GdyxiM4D36YqR84aoBfWiAtUciBVbXugwjZJnuVe2dQN -k ~/.config/solana/id.json -r https://testnet.dev2.eclipsenetwork.xyz  -p 0 -n 1
