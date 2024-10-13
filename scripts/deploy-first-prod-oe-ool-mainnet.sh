83e6104b18dcf3976920983b3b837bc0a41de822
Vadim Fadeev <xboxfadeev@gmail.com>
2024-10-13 23:09:40 +0100

    feat: add ledger

--- scripts/deploy-first-prod-oe-ool-mainnet.sh
+++ scripts/deploy-first-prod-oe-ool-mainnet.sh
@@ -14,8 +14,9 @@ npx ts-node ./src/cli/controls/createDeployment.ts \
   --platformFeeRecipients 4yyE2cWHJTU5cu8pem2ApVnHRDGHYvsPvsFCM2WeCPG2:100 \
   --isFeeFlat \
   --itemBaseUri "ipfs://QmboLkdYkaunVsXDcfVQRKo9wysRk22z4nvvcpvoNbkPX1" \
-  --itemName "Over Open Land"
+  --itemName "Over Open Land" \
+  --ledger true
 
-npx ts-node ./src/cli/controls/addPhase.ts -d 8iHEUGSycmHcCDfBtZgNVgTm5WGgsRaiYxtDpnYMCAzq -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz --maxMintsPerWallet 100 --maxMintsTotal 0 --priceAmount 0 -s 1729000800 -e 1729432800
+npx ts-node ./src/cli/controls/addPhase.ts -d 8iHEUGSycmHcCDfBtZgNVgTm5WGgsRaiYxtDpnYMCAzq -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz --maxMintsPerWallet 100 --maxMintsTotal 0 --priceAmount 0 -s 1729000800 -e 1729432800 --ledger true
 
-npx ts-node ./src/cli/controls/mintWithControls.ts -d 8iHEUGSycmHcCDfBtZgNVgTm5WGgsRaiYxtDpnYMCAzq -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz -p 0 -n 1
+npx ts-node ./src/cli/controls/mintWithControls.ts -d 8iHEUGSycmHcCDfBtZgNVgTm5WGgsRaiYxtDpnYMCAzq -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz -p 0 -n 1 --ledger true
