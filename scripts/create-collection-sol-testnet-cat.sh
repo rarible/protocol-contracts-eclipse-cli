38aea178b76e95e1df18b26989486d3d4d219c10
Vadim Fadeev <xboxfadeev@gmail.com>
2024-10-13 23:22:22 +0100

    feat: add new deploy script

---
+++ scripts/create-collection-sol-testnet-cat.sh
@@ -0,0 +1,23 @@
+# test network
+
+npx ts-node ./src/cli/controls/createDeployment.ts \
+  -t BTicWgGhoTsBANuirXS7UCeF6bxfvTnCaxM5HQRRfGZS \
+  -k ~/.config/solana/id.json \
+  -r https://api.devnet.solana.com \
+  -s CAT5 \
+  --maxNumberOfTokens 1000 \
+  --maxMintsPerWallet 100 \
+  -j "ipfs://QmfJh4B8KySR1KHaXRNWkcDBn67ZuJkzAyaVCWAS8Kcezc/0" \
+  -n "Collection of Cats Two Phases" \
+  --creators J5xffSinbAQw65TsphSZ8gfaNGAPEfNWL9wwzGNdm3PR:100 \
+  --royaltyBasisPoints 1000 \
+  --platformFeeValue 500000 \
+  --platformFeeRecipients 4yyE2cWHJTU5cu8pem2ApVnHRDGHYvsPvsFCM2WeCPG2:50 J5xffSinbAQw65TsphSZ8gfaNGAPEfNWL9wwzGNdm3PR:50 \
+  --isFeeFlat \
+  --extraMeta "type:handmade" "author:Vadim" "value:important" \
+  --itemBaseUri "ipfs://QmdHaufjUDJgbZzZ4eFCjtJQyeQpuNwoEvqLm5rq159vC8/{}" \
+  --itemName "Test Cat #{}"
+
+npx ts-node ./src/cli/controls/addPhase.ts -d DeJERCMPzrow46iL2NgHK2H1EFUTqUrzmH9vuWvGmMPL -k ~/.config/solana/id.json -r https://api.devnet.solana.com --maxMintsPerWallet 100 --maxMintsTotal 1000 --priceAmount 250 -s 1709564319 -e 1959564319
+
+npx ts-node ./src/cli/controls/mintWithControls.ts -d DeJERCMPzrow46iL2NgHK2H1EFUTqUrzmH9vuWvGmMPL -k ~/.config/solana/id.json -r https://api.devnet.solana.com -p 0 -n 1
