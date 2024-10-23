16b73ef1543a3053604a4547eb6240326d8c7079
Vadim Fadeev <xboxfadeev@gmail.com>
2024-10-23 12:37:25 +0100

    Feature/eclipse lts v2 (#6)
    * feat: add ledger

--- scripts/deploy-first-prod-oe-ool-mainnet.sh
+++ scripts/deploy-first-prod-oe-ool-mainnet.sh
@@ -1,21 +1,22 @@
-# change OOLT to without t
+# change POD
 npx ts-node ./src/cli/controls/createDeployment.ts \
-  -t 4yyE2cWHJTU5cu8pem2ApVnHRDGHYvsPvsFCM2WeCPG2 \
+  -t AsSKqK7CkxFUf3KaoQzzr8ZLPm5fFguUtVE5QwGALQQn \
   -k ~/.config/solana/id.json \
   -r https://mainnetbeta-rpc.eclipse.xyz \
-  -s OOL \
+  -s POD \
   --maxNumberOfTokens 0 \
-  --maxMintsPerWallet 100 \
-  -u "ipfs://QmYFLidfYnAcWPUWT1XkimVN7KKrs1n7P76xwWaQGvMu8i" \
-  -n "Over Open Land" \
-  --creators J5xffSinbAQw65TsphSZ8gfaNGAPEfNWL9wwzGNdm3PR:100 \
+  --maxMintsPerWallet 500 \
+  -j "ipfs://QmTmNdx3cNT4wv5yyi7ajvPLZTVia6QzWzg1AAkKtauqqs" \
+  -n "Path of Discovery" \
+  --creators AsSKqK7CkxFUf3KaoQzzr8ZLPm5fFguUtVE5QwGALQQn:100 \
   --royaltyBasisPoints 5000 \
   --platformFeeValue 81830 \
-  --platformFeeRecipients 4yyE2cWHJTU5cu8pem2ApVnHRDGHYvsPvsFCM2WeCPG2:100 \
+  --platformFeeRecipients AsSKqK7CkxFUf3KaoQzzr8ZLPm5fFguUtVE5QwGALQQn:100 \
   --isFeeFlat \
-  --itemBaseUri "ipfs://QmboLkdYkaunVsXDcfVQRKo9wysRk22z4nvvcpvoNbkPX1" \
-  --itemBaseName "Over Open Land"
+  --itemBaseUri "ipfs://QmTmNdx3cNT4wv5yyi7ajvPLZTVia6QzWzg1AAkKtauqqs" \
+  --itemName "Path of Discovery" \
+  --ledger true
 
-npx ts-node ./src/cli/controls/addPhase.ts -d 8iHEUGSycmHcCDfBtZgNVgTm5WGgsRaiYxtDpnYMCAzq -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz --maxMintsPerWallet 100 --maxMintsTotal 0 --priceAmount 0 -s 1729000800 -e 1729432800
+npx ts-node ./src/cli/controls/addPhase.ts -d 6wBjjwmWAZzuzAYt9N7EbhPsthE6m2LStNAxZUbViqQb -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz --maxMintsPerWallet 100 --maxMintsTotal 0 --priceAmount 0 -s 1729173600 -e 1729778400 --ledger true
 
-npx ts-node ./src/cli/controls/mintWithControls.ts -d 8iHEUGSycmHcCDfBtZgNVgTm5WGgsRaiYxtDpnYMCAzq -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz -p 0 -n 1
+npx ts-node ./src/cli/controls/mintWithControls.ts -d 6wBjjwmWAZzuzAYt9N7EbhPsthE6m2LStNAxZUbViqQb -k ~/.config/solana/id.json -r https://mainnetbeta-rpc.eclipse.xyz -p 0 -n 1 --ledger true
